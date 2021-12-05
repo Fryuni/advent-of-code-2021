3d8267162ea2a762eb6e1ecba8593e479b712721
Luiz Ferraz <luiz@lferraz.com>
2021-12-05 16:10:54 -0300

    Day two

--- src/day2/main.rs
+++ src/day2/main.rs
@@ -1,18 +1,59 @@
 //! Binary for solving day 2 of Advent of Code 2021
 
-use anyhow::Context;
+use anyhow::{Context, Error};
 use aoc2021::InputProvider;
 use include_dir::*;
-use itertools::Itertools;
+use std::str::FromStr;
 
 static INPUT_DIR: Dir = include_dir!("$CARGO_MANIFEST_DIR/src/day2/input");
 
-fn challenge_one(input: &str) -> anyhow::Result<usize> {
-    Ok(0)
+#[derive(Debug)]
+enum Instruction {
+    Forward(usize),
+    Down(usize),
+    Up(usize),
 }
 
-fn challenge_two(input: &str) -> anyhow::Result<usize> {
-    Ok(0)
+impl FromStr for Instruction {
+    type Err = Error;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let err_closure = || format!("malformed instruction ({})", s);
+        let (name, value) = s.split_once(' ').with_context(err_closure)?;
+
+        let value = value.parse().with_context(err_closure)?;
+
+        match name {
+            "forward" => Ok(Self::Forward(value)),
+            "down" => Ok(Self::Down(value)),
+            "up" => Ok(Self::Up(value)),
+            _ => Err(Error::msg(err_closure())),
+        }
+    }
+}
+
+fn challenge_one(input: &[Instruction]) -> anyhow::Result<usize> {
+    let (h, v) = input
+        .iter()
+        .fold((0, 0), |(h, v), instruction| match instruction {
+            Instruction::Forward(x) => (h + x, v),
+            Instruction::Down(x) => (h, v + x),
+            Instruction::Up(x) => (h, v - x),
+        });
+
+    Ok(h * v)
+}
+
+fn challenge_two(input: &[Instruction]) -> anyhow::Result<usize> {
+    let (h, v, _) = input
+        .iter()
+        .fold((0, 0, 0), |(h, v, aim), instruction| match instruction {
+            Instruction::Forward(x) => (h + x, v + (aim * x), aim),
+            Instruction::Down(x) => (h, v, aim + x),
+            Instruction::Up(x) => (h, v, aim - x),
+        });
+
+    Ok(h * v)
 }
 
 fn process(name: &str) -> anyhow::Result<()> {
@@ -20,16 +61,18 @@ fn process(name: &str) -> anyhow::Result<()> {
         .get_input(&format!("{}.txt", name))
         .context("reading content")?;
 
+    let input: Vec<Instruction> = content.lines().map(str::parse).collect::<Result<_, _>>()?;
+
     println!(
         "Challenge one ({}): {}",
         name,
-        challenge_one(content).context("challenge one")?
+        challenge_one(&input).context("challenge one")?
     );
 
     println!(
         "Challenge two ({}): {}",
         name,
-        challenge_two(content).context("challenge two")?
+        challenge_two(&input).context("challenge two")?
     );
 
     Ok(())
